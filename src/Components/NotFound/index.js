63db62c5577ec7d0d32693bfe2e10f764a10cec4
rajasheskar99 <141818030+rajasheskar99@users.noreply.github.com>
2023-09-04 01:21:11 +0530

    Update index.js

--- src/Components/NotFound/index.js
+++ src/Components/NotFound/index.js
@@ -1,17 +1,24 @@
+import Header from '../Header'
 import './index.css'
 
 const NotFound = () => (
-  <div className="found-sec">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
-      className="no-found"
-      alt="notfound"
-    />
-    <h1 className="found-head"> Page Not Found</h1>
-    <p className="try">
-      We are sorry, the page you requested could not be found
-    </p>
-  </div>
+  <>
+    <Header />
+    <div className="found-sec">
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
+          className="no-found-image"
+          alt="not found"
+        />
+      </div>
+
+      <h1 className="found-head"> Page Not Found</h1>
+      <p className="not-found-desc">
+        We are sorry, the page you requested could not be found
+      </p>
+    </div>
+  </>
 )
 
 export default NotFound
